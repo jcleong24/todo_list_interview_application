fc3d28bf76ad7d02a48b7b021208b35961e14126
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-06-11 19:25:27 +0800

    add - dummy data todoItem

---
+++ app/data/dummy_todos.ts
@@ -0,0 +1,42 @@
+import type { TodoItem } from "@/app//types/todo";
+
+// export interface TodoItem {
+//     id: string;
+//     title: string;
+//     description: string;
+//     isCompleted: boolean;
+//     dueDate: string;
+//     category: Category;
+//     createdAt: string;
+//     status: TodoStatus;
+// }
+
+export const todoItems: TodoItem[] = [
+  {
+    id: "1",
+    title: "Create a React project",
+    description: "I am creating a React Project",
+    isCompleted: false,
+    category: "Work",
+    createdAt: "5:23 AM, 01/06/2022",
+    status: "pending",
+  },
+  {
+    id: "2",
+    title: "Learn React",
+    description: "I am learning a React Project",
+    isCompleted: false,
+    category: "Work",
+    createdAt: "5:22 AM, 01/06/2022",
+    status: "pending",
+  },
+  {
+    id: "3",
+    title: "Create a Todo App",
+    description: "I am creating a Todo List Project",
+    isCompleted: false,
+    category: "Work",
+    createdAt: "5:21 AM, 01/06/2022",
+    status: "completed",
+  },
+];
